d0afff81887e3ccda36da2477ebaaf570a5366ef
ms36 <mstevenson36@hotmail.com>
2020-09-17 18:06:12 -0400

    updated page buttons

--- angular-munchables/src/app/app.component.ts
+++ angular-munchables/src/app/app.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-munchables';
-  pageNumber = [1, 2] ;
+  leftPageNumber = 1;
+  rightPageNumber = 2;
+  numberOfRecipes = 5;
+
+  turnPage(direction: string) {
+    if (direction === 'left') {
+      this.leftPageNumber -= 2;
+      this.rightPageNumber -= 2;
+    } else if (direction === 'right') {
+      this.leftPageNumber += 2;
+      this.rightPageNumber += 2;
+    }
+  }
 }
